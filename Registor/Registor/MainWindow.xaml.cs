9f95ebad5656c1e301d9f8259d4515528ba567a9
Hikari99MOAV <78300258+Hikari99MOAV@users.noreply.github.com>
2022-04-23 15:03:45 +0300

    Add files via upload

---
+++ Registor/Registor/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Registor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenPage(pages.login);
+        }
+        public enum pages
+        {
+            login,
+            regin
+        }
+       public void OpenPage(pages pages)
+        {
+            if (pages == pages.login)
+            {
+                frame.Navigate(new login(this));
+            }
+            else if (pages == pages.regin)
+                frame.Navigate(new regin(this));
+        }
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("server=ASUS;Trusted_Connection=Yes;DataBase=ComputerShop;");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            return dataTable;
+        }
+
+       
+    }
+}
---
+++ Registor/Registor/login.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registor
+{
+    /// <summary>
+    /// Логика взаимодействия для login.xaml
+    /// </summary>
+    public partial class login : Page
+    {
+        public MainWindow mainWindow;
+        public login(MainWindow _mainWindow)
+        {
+            InitializeComponent();
+            mainWindow = _mainWindow;
+        }
+
+       
+
+        private void regin_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.OpenPage(MainWindow.pages.regin);
+        }
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (textBox_login.Text.Length > 0) // проверяем введён ли логин     
+            {
+                if (password.Password.Length > 0) // проверяем введён ли пароль         
+                {             // ищем в базе данных пользователя с такими данными         
+                    DataTable dt_user = mainWindow.Select("SELECT * FROM [dbo].[User] WHERE [login] = '" + textBox_login.Text + "' AND [password] = '" + password.Password + "'");
+                    if (dt_user.Rows.Count > 0) // если такая запись существует       
+                    {
+                        MessageBox.Show("Пользователь авторизовался"); // говорим, что авторизовался
+                        WindowMenu windowMenu= new WindowMenu();
+                        windowMenu.ShowDialog();
+                    }
+                    else MessageBox.Show("Пользователя не найден"); // выводим ошибку  
+                }
+                else MessageBox.Show("Введите пароль"); // выводим ошибку    
+            }
+            else MessageBox.Show("Введите логин"); // выводим ошибку 
+        }
+    }
+}
